5761e33c7b5e8ca958ddedae847e0870c5c2360c
Trade Unifox <tradeunifox+github@gmail.com>
2025-10-27 11:33:31 +0530

    Delete build/lib directory

--- build/lib/Whatsapp/__init__.py
+++
@@ -1,9 +0,0 @@
-from .token_manager import TokenManager
-from .message_sender import MessageSender
-from .message_receiver import MessageReceiver
-
-__all__ = ["TokenManager", "MessageSender", "MessageReceiver"]
-
-
-__version__ = "0.0.1"
-__author__ = "Trade Unifox"
